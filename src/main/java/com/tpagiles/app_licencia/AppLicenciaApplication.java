8fbdcdb19a0e20f88ceae6eaae166582fde0062c
Marcos Joaquin Poet <mpoet@frsf.utn.edu.ar>
2025-05-01 21:55:21 -0300

    Initial commit

---
+++ src/main/java/com/tpagiles/app_licencia/AppLicenciaApplication.java
@@ -0,0 +1,13 @@
+package com.tpagiles.app_licencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLicenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppLicenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tpagiles/app_licencia/AppLicenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpagiles.app_licencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppLicenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
