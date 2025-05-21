8ddac27e0a71e5039053bf1cd29d94db5c0d6308
Marcos Pividori <mpividori3@gmail.com>
2025-05-20 21:50:27 -0300

    fix: fix: crear tabla tarifario_licencia si no existe antes de insertar datos

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,10 @@
+CREATE TABLE IF NOT EXISTS tarifario_licencia (
+                                                  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                                  clase_licencia VARCHAR(2) NOT NULL,
+                                                  vigencia_anios INT NOT NULL,
+                                                  costo DOUBLE NOT NULL
+);
+
 INSERT INTO tarifario_licencia (clase_licencia, vigencia_anios, costo) VALUES
                                                                            ('A', 5, 40.0), ('A', 4, 30.0), ('A', 3, 25.0), ('A', 1, 20.0),
                                                                            ('B', 5, 40.0), ('B', 4, 30.0), ('B', 3, 25.0), ('B', 1, 20.0),
