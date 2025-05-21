f5a9719906b76a4a61356abe124b9ad77a1ddab3
MarcosPiv <mpividori3@gmail.com>
2025-05-21 18:41:30 -0300

    feat: agregacion de CORS para comunicacion con el frontend

--- src/main/java/com/tpagiles/app_licencia/AppLicenciaApplication.java
+++ src/main/java/com/tpagiles/app_licencia/AppLicenciaApplication.java
@@ -11,6 +11,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.LocalDate;
 
@@ -79,6 +81,19 @@ public class AppLicenciaApplication {
 				repo.save(new TarifarioLicencia(null, ClaseLicencia.G, 4, 30.0));
 				repo.save(new TarifarioLicencia(null, ClaseLicencia.G, 3, 25.0));
 				repo.save(new TarifarioLicencia(null, ClaseLicencia.G, 1, 20.0));
+			}
+		};
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
 			}
 		};
 	}
